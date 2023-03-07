1ec741648b42a9bb80308adc312b10f3c7605a5b
triet <pvmtriet232@gmail.com>
2023-03-08 00:25:01 +0700

    fix package

--- main.go
+++ main.go
@@ -1,60 +1,60 @@
 package main
 
 import (
-  "os"
-  "os/signal"
-  "syscall"
+	"os"
+	"os/signal"
+	"syscall"
 
-  // Make sure you change this line to match your module
-  "github.com/pvmtriet232/go-and-compose/apiserver"
-  "github.com/sirupsen/logrus"
-  "github.com/urfave/cli/v2"
+	// Make sure you change this line to match your module
+	"github.com/pvmtriet232/go-compose-practise/apiserver"
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
 )
 
 const (
-  apiServerAddrFlagName string = "addr"
+	apiServerAddrFlagName string = "addr"
 )
 
 func main() {
-  if err := app().Run(os.Args); err != nil {
-    logrus.WithError(err).Fatal("could not run application")
-  }
+	if err := app().Run(os.Args); err != nil {
+		logrus.WithError(err).Fatal("could not run application")
+	}
 }
 
 func app() *cli.App {
-  return &cli.App{
-    Name:  "api-server",
-    Usage: "The API",
-    Commands: []*cli.Command{
-      apiServerCmd(),
-    },
-  }
+	return &cli.App{
+		Name:  "api-server",
+		Usage: "The API",
+		Commands: []*cli.Command{
+			apiServerCmd(),
+		},
+	}
 }
 
 func apiServerCmd() *cli.Command {
-  return &cli.Command{
-    Name:  "start",
-    Usage: "starts the API server",
-    Flags: []cli.Flag{
-      &cli.StringFlag{Name: apiServerAddrFlagName, EnvVars: []string{"API_SERVER_ADDR"}},
-    },
-    Action: func(c *cli.Context) error {
-      done := make(chan os.Signal, 1)
-      signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	return &cli.Command{
+		Name:  "start",
+		Usage: "starts the API server",
+		Flags: []cli.Flag{
+			&cli.StringFlag{Name: apiServerAddrFlagName, EnvVars: []string{"API_SERVER_ADDR"}},
+		},
+		Action: func(c *cli.Context) error {
+			done := make(chan os.Signal, 1)
+			signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
-      stopper := make(chan struct{})
-      go func() {
-        <-done
-        close(stopper)
-      }()
+			stopper := make(chan struct{})
+			go func() {
+				<-done
+				close(stopper)
+			}()
 
-      addr := c.String(apiServerAddrFlagName)
-      server, err := apiserver.NewAPIServer(addr)
-      if err != nil {
-        return err
-      }
+			addr := c.String(apiServerAddrFlagName)
+			server, err := apiserver.NewAPIServer(addr)
+			if err != nil {
+				return err
+			}
 
-      return server.Start(stopper)
-    },
-  }
+			return server.Start(stopper)
+		},
+	}
 }

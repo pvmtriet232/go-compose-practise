4a2edee6ca512726a98220f4774f259ff058906a
triet <pvmtriet232@gmail.com>
2023-03-08 00:22:42 +0700

    change

--- apiserver/apiserver.go
+++ apiserver/apiserver.go
@@ -2,61 +2,61 @@
 package apiserver
 
 import (
-  "context"
-  "errors"
-  "net/http"
-  "time"
+	"context"
+	"errors"
+	"net/http"
+	"time"
 
-  "github.com/gorilla/mux"
-  "github.com/sirupsen/logrus"
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 )
 
 var defaultStopTimeout = time.Second * 30
 
 type APIServer struct {
-  addr string
+	addr string
 }
 
 func NewAPIServer(addr string) (*APIServer, error) {
-  if addr == "" {
-    return nil, errors.New("addr cannot be blank")
-  }
+	if addr == "" {
+		return nil, errors.New("addr cannot be blank")
+	}
 
-  return &APIServer{
-    addr: addr,
-  }, nil
+	return &APIServer{
+		addr: addr,
+	}, nil
 }
 
 // Start starts a server with a stop channel
 func (s *APIServer) Start(stop <-chan struct{}) error {
-  srv := &http.Server{
-    Addr:    s.addr,
-    Handler: s.router(),
-  }
+	srv := &http.Server{
+		Addr:    s.addr,
+		Handler: s.router(),
+	}
 
-  go func() {
-    logrus.WithField("addr", srv.Addr).Info("starting server")
-    if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-      logrus.Fatalf("listen: %s\n", err)
-    }
-  }()
+	go func() {
+		logrus.WithField("addr", srv.Addr).Info("starting server")
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logrus.Fatalf("listen: %s\n", err)
+		}
+	}()
 
-  <-stop
-  ctx, cancel := context.WithTimeout(context.Background(), defaultStopTimeout)
-  defer cancel()
+	<-stop
+	ctx, cancel := context.WithTimeout(context.Background(), defaultStopTimeout)
+	defer cancel()
 
-  logrus.WithField("timeout", defaultStopTimeout).Info("stopping server")
-  return srv.Shutdown(ctx)
+	logrus.WithField("timeout", defaultStopTimeout).Info("stopping server")
+	return srv.Shutdown(ctx)
 }
 
 func (s *APIServer) router() http.Handler {
-  router := mux.NewRouter()
+	router := mux.NewRouter()
 
-  router.HandleFunc("/", s.defaultRoute)
-  return router
+	router.HandleFunc("/", s.defaultRoute)
+	return router
 }
 
 func (s *APIServer) defaultRoute(w http.ResponseWriter, r *http.Request) {
-  w.WriteHeader(http.StatusOK)
-  w.Write([]byte("Hello triet"))
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello tao ne "))
 }
